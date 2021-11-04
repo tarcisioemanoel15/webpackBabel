483b53f0aeb3b8bceedee33efe60d6bfa15fccde
tarcisioemanoel15 <caninosbrancos10031@gmail.com>
2021-11-03 22:44:18 -0200

    finalizadowebpackbabel

---
+++ src/main.js
@@ -0,0 +1,10 @@
+// import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+import './assets/css/style.css';
+import {dizoi} from './modules/test1';
+
+
+
+console.log('ola mundo');
+dizoi();
---
+++ src/modules/test1.js
@@ -0,0 +1,5 @@
+export function dizoi(){
+    console.log('oiiiiiiii');
+    console.log('ooooooi');
+}
+
